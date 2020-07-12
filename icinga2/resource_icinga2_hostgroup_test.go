420272a8f9e567821aec40a980c5cc60360cef50
Hugo Soto <hugo@preyhq.com>
2020-07-12 13:14:24 -0400

    hostgroup: move tf resource to func tpl

--- icinga2/resource_icinga2_hostgroup_test.go
+++ icinga2/resource_icinga2_hostgroup_test.go
@@ -10,12 +10,13 @@ import (
 )
 
 func TestAccCreateBasicHostGroup(t *testing.T) {
+	var (
+		hostgroup iapi.HostgroupStruct
 
-	var testAccCreateBasicHostGroup = fmt.Sprintf(`
-		resource "icinga2_hostgroup" "tf-hg-1" {
-		  name = "terraform-hostgroup-1"
-		  display_name = "Terraform Test HostGroup"
-	}`)
+		hostgroupName     = "terraform-hostgroup-1"
+		firstDisplayName  = "Terraform Test HostGroup"
+		secondDisplayName = "Some New HostGroup DisplayName"
+	)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
@@ -52,4 +53,13 @@ func testAccCheckHostgroupExists(rn string) resource.TestCheckFunc {
 
 		return nil
 	}
+}
+
+func testAccCreateHostGroupBasic(name, displayName string) string {
+	return fmt.Sprintf(`
+resource "icinga2_hostgroup" "tf-hg-1" {
+	name = "%s"
+	display_name = "%s"
+}
+`, name, displayName)
 }
