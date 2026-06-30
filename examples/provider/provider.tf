12dbfab331790c8342a70ddaecdcefb9fecb1bb0
Bram Vogelaar <bram@attachmentgenie.com>
2026-06-30 16:22:17 +0200

    fix: cleaning up example code
    Signed-off-by: Bram Vogelaar <bram@attachmentgenie.com>

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,6 +1,6 @@
 x# Configure the Icinga2 provider
 provider "icinga2" {
-  api_url                  = "https://192.168.33.5:5665/v1"
+  api_url                  = "https://localhost:5665/v1"
   api_user                 = "root"
   api_password             = "icinga"
   insecure_skip_tls_verify = true
